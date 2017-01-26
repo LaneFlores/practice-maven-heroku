d4b66eea7a4e5543c1bbc2b77b7d92e7f38950bd
LaneFlores <mflores@star2star.com>
2017-01-26 14:20:47 -0500

    Continuous Integration Testing - Breaking It
    Breaking the build intentionally to see if CI is working.

--- src/main/java/com/practice/heroku/util/PracticeUtilityClass.java
+++ src/main/java/com/practice/heroku/util/PracticeUtilityClass.java
@@ -5,7 +5,7 @@ public class PracticeUtilityClass {
         // We're going to have some tests on this function, which expect it to return the higher of the two numbers.
         // HOWEVER, to test our continuous integration, we're going to break it.
 
-        // return x; // This line breaks it.
-        return Math.max(x, y); // This line fixes it.
+        return x; // This line breaks it.
+        //return Math.max(x, y); // This line fixes it.
     }
 }
