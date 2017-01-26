12669b7648aa2706017067f0638b9712c5d6ba8b
LaneFlores <mflores@star2star.com>
2017-01-26 14:16:37 -0500

    Continuous Integration Testing

---
+++ src/main/java/com/practice/heroku/util/PracticeUtilityClass.java
@@ -0,0 +1,11 @@
+package com.practice.heroku.util;
+
+public class PracticeUtilityClass {
+    public static int max(int x, int y) {
+        // We're going to have some tests on this function, which expect it to return the higher of the two numbers.
+        // HOWEVER, to test our continuous integration, we're going to break it.
+
+        // return x; // This line breaks it.
+        return Math.max(x, y); // This line fixes it.
+    }
+}
---
+++ src/test/java/com/practice/heroku/util/TestPracticeUtilityClass.java
@@ -0,0 +1,16 @@
+package com.practice.heroku.util;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestPracticeUtilityClass {
+
+    @Test public void testMaxLeft() { runTest(2,1,2); }
+    @Test public void testMaxRight() { runTest(1,2,2); }
+    @Test public void testMaxEqual() { runTest(1,1,1); }
+
+    private void runTest(int x, int y, int expected) {
+        Assert.assertEquals(expected, PracticeUtilityClass.max(x,y));
+    }
+}
